c15b25564bb033bf80b2b4e108316c7d831cb71e
Emad Rahman <emadpoke@gmail.com>
2018-02-17 17:11:20 -0800

    add table shemas

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,21 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+
+class Destinations(models.Model):
+    title = models.CharField(max_length=50)
+    price = models.FloatField()
+    photoURL = models.CharField(max_length=300)
+    description = models.CharField(max_length=1000)
+    city_id = models.IntegerField()
+
+
+class Time(models.Model):
+    time = models.DateTimeField()
+    city_id = models.IntegerField()
+
+
+class Indoor(models.Model):
+    isIndoor = models.BooleanField()
+    city_id = models.IntegerField()
